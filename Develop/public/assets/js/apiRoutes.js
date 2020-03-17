8f0b7e3c842d946fd1b3b2911804bc6625187381
SpencerFW <srfalorward@ucdavis.edu>
2020-03-17 00:00:25 -0700

    more code to apiRoutes and server js files

--- Develop/public/assets/js/apiRoutes.js
+++ Develop/public/assets/js/apiRoutes.js
@@ -1,13 +1,26 @@
 const noteData = require('../../../db/db');
-// const indexData = require('../../../public/index.html');
+
 
 module.exports = function(app) {
-	app.get(' /api/notes', function(req, res) {
+    //displays all notes
+	app.get('/api/notes', function(req, res) {
 		res.json(noteData);
 	});
 	app.post('/api/notes', function(req, res) {
-		noteData.push(req.body);
-		// res.json(true)
+		const newNote = req.body;
+		//adds the JSON the user sent
+		noteData.push(newNote);
+
+		console.log(newNote);
+		//displays the JSON to the user
+		res.json(newNote);
+    });
+    // delets note with 'id'
+	app.delete('/api/notes/:id', function(req, res) {
+		const deleteNote = req.params.id;
+
+		console.log(deleteNote);
+
+		res.end();
 	});
-	app.delete('/api/notes/:id', function(req, res) {});
 };
