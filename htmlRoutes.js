704705d1c827d0435c796e55226d63958caab5a9
SpencerFW <srfalorward@ucdavis.edu>
2020-03-19 10:13:13 -0700

    commented out test routes

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -2,29 +2,44 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
-var path = require('path');
-var public = path.join(__dirname, './public');
+var path = require('path')
+var public = path.join(__dirname, './public')
+// var root = path.join(__dirname, '/')
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
-	// HTML GET Requests
-	// Below code handles when users "visit" a page.
-	// In each of the below cases the user is shown an HTML page of content
-	// ---------------------------------------------------------------------------
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
 
-	app.get('/index', function(req, res) {
-		res.sendFile(path.join(public, 'index.html'));
-	});
+    app.get('/index', function(req, res) {
+        res.sendFile(path.join(public, 'index.html'))
+    })
 
-	app.get('/notes', function(req, res) {
-		res.sendFile(path.join(public, 'notes.html'));
-	});
+    app.get('/notes', function(req, res) {
+        res.sendFile(path.join(public, 'notes.html'))
+    })
 
-	// If no matching route is found default to index
-	app.get('*', function(req, res) {
-		res.sendFile(path.join(public, 'index.html'));
-	});
-};
-console.log('Test');
+    // If no matching route is found default to index
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(public, 'index.html'))
+    })
+
+    // //test routes
+    // app.get('/index', function(req, res) {
+    //     res.sendFile(path.join(root, 'index.html'))
+    // })
+
+    // app.get('/notes', function(req, res) {
+    //     res.sendFile(path.join(root, 'notes.html'))
+    // })
+
+    // // If no matching route is found default to index
+    // app.get('*', function(req, res) {
+    //     res.sendFile(path.join(root, 'index.html'))
+    // })
+}
+console.log('Test')
