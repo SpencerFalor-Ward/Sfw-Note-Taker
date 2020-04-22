e9c52ab12745434136f91678078df875beeaa0c0
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-04-22 14:29:03 -0700

    fixed api path code from Nico

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,59 +1,73 @@
 var path = require('path')
 var fs = require('fs')
-var noteData = path.join(__dirname, './db/db.json') //returns a string ?
-var data = fs.readFileSync(noteData, 'utf8') //returns a string
+var noteData = path.join(__dirname, './db/db.json') //returns a string
 
 module.exports = function(app) {
     //displays all notes. The readFileSync does this synchronously so that it happens before other functions run to make sure data is present
-    app.get('/api/notes', function(req, res) {
-        let note = data ? JSON.parse(data) : { message: 'No notes found' }
-        res.json(note) // response returns note as a string
-    })
+    var notes;
+fs.readFile(noteData, "utf8", function(err, data) {
+  notes = JSON.parse(data);
+});
 
-    app.post('/api/notes', function(req, res) {
-        var newNote = req.body // express parses the data into an object. this pulls all the information from the ajax request
-		noteObj = JSON.parse(data) // turns noteData string into an array of objects
+app.get("/api/notes", function(req, res) {
+  res.json(notes);
+});
 
-        //need the length of object array for later code ?, either 1 or current length
-		let noteLength = noteObj === 0 ? 1 : noteObj[noteObj.length - 1].id + 1
+app.post("/api/notes", function(req, res) {
+  var newNote = req.body;
+  newNote.id = notes.length;
+  notes.push(newNote);
+  saveNotes();
+  res.json({ ok: true });
+});
 
-		//adds the JSON the user sent
-		//could not use req.body with forEach becasue it is an object and forEach can only be run on an array
-		Object.keys(newNote).forEach(key => {
-            newNote[key]
-		})
-        // newNote.forEach(el => {
-        //     el.id = id
-        //     noteObj.push(el)
-        //     id++
-		// })
+app.delete("/api/notes/:id", function(req, res) {
+  notes = notes.filter(note => note.id !== parseInt(req.params.id));
+  saveNotes();
+  res.json({ ok: true });
+});
 
-        console.log(newNote)
-
-        // write the new note
-        fs.writeFile(noteData, JSON.stringify(noteObj), err => {
-            if (err) console.log("Didn't write note")
-        })
-        res.json(newNote)
-        return
-    })
-
-    // delets note with 'id'
-    app.delete('/api/notes/:id', function(req, res) {
-        var deleteNote = req.params.id // changes obj into string
-        data = JSON.parse(data) // changes data into obj
-        let currentNote = data.filter(
-            currentNote => currentNote.id == parseInt(deleteNote)
-        )
-        let noteIndex = data.indexOf(currentNote)
-        if (noteIndex > -1) {
-            data.splice(noteIndex, 1)
-            data, JSON.stringify(data)
-            res, json(currentNote)
-        }
-        console.log(deleteNote)
-        res.json(currentNote)
-        return
-    })
+function saveNotes() {
+  fs.writeFile("db/db.json", JSON.stringify(notes), function(err) {
+    if (err) throw err;
+    console.log("notes saved");
+  });
 }
-console.log('Test')
+    // app.get('/api/notes', function(req, res) {
+
+    //     console.log('Hi')
+    //     fs.readFile(noteData, (err, notes) => {
+    //         res.json(JSON.parse(notes))
+    //     })
+    //     console.log('hello')
+    // })
+    // app.post('/api/notes', function(req, res) {
+    //     var data = fs.readFileSync(noteData, 'utf8')
+    //     var newNote = req.body // express parses the data into an object
+    //     var noteObj = JSON.parse(data) // turns noteData string into an array of objects
+    //     newNote.id = noteObj.length
+    //     noteObj.push(newNote)
+    //     // write the new note
+    //     fs.writeFile(noteData, JSON.stringify(noteObj), err => {
+    //         if (err) console.log("Didn't write note")
+    //         res.json(newNote)
+    //     })
+    // })
+    // // delets note with 'id'
+    // app.delete('/api/notes/:id', function(req, res) {
+    //     var notePath = path.join(__dirname, './db/db.json')
+    //     var data = fs.readFileSync(notePath, 'utf8')
+    //     var deleteNote = req.params.id // changes obj into string
+    //     var noteData = JSON.parse(data) // changes data inot obj
+    //     let keepNotes = noteData.filter(note => {
+    //         return note.id != parseInt(deleteNote)
+    //     })
+    //     fs.writeFile(
+    //         'db/db.json',
+    //         JSON.stringify(keepNotes),
+    //         (err, response) => {
+    //             res.json({ ok: true })
+    //         }
+    //     )
+    // })
+}
